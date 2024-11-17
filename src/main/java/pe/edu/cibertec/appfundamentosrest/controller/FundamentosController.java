45344a729550b48b680327d13eb7edb63659d0a5
sebastianfabrizio <sebasotaradiaz@gmail.com>
2024-11-17 10:41:21 -0500

    Ejercicios con path variable y request param

--- src/main/java/pe/edu/cibertec/appfundamentosrest/controller/FundamentosController.java
+++ src/main/java/pe/edu/cibertec/appfundamentosrest/controller/FundamentosController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import pe.edu.cibertec.appfundamentosrest.service.FundamentosService;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -45,12 +46,36 @@ public class FundamentosController {
     }
 
 
+    //localhost:8080/api/v1/fundamentos/hola
+    //localhost:8080/api/v1/fundamentos/hola/sebas
     @GetMapping({"/hola", "hola/{nombre}"})
     public ResponseEntity<String> hola(@PathVariable(required = false) String nombre){
         if(nombre == null){
             return new ResponseEntity<>("Hola Mundo Rest", HttpStatus.OK);
         }
         return new ResponseEntity<>("hola "+nombre, HttpStatus.OK);
+    }
+
+    //localhost:8080/api/v1/fundamentos/potencia=?numero=8&exponente=3
+    @GetMapping("/potencia")
+    public ResponseEntity<String> potencia(@RequestParam(required = false,
+            defaultValue = "2") int numero, @RequestParam(required = false,
+            defaultValue = "2") int exponente){
+        double valorPotencia = Math.pow(numero, exponente);
+        return new ResponseEntity<>("El resultado es ="+ valorPotencia, HttpStatus.OK);
+    }
+
+    @GetMapping({"/generar", "generar/{inicio}"})
+    public ResponseEntity<List<Integer>> generar(  @PathVariable(required = false) Integer inicio,
+                                                   @RequestParam(required = false, defaultValue = "10") Integer fin){
+      if(inicio == null){
+          inicio = 1;
+      }
+      List<Integer> rango = new ArrayList<>();
+      for(int i = inicio; i <=fin; i++){
+          rango.add(i);
+      }
+      return new ResponseEntity<>(rango, HttpStatus.OK);
     }
 
 }
